8f1340577a50ea351b05ec69d8d6b0ec1c4ca5cc
d.protsiuk@gmail.com <d.protsiuk@gmail.com>
2018-05-08 12:32:15 +0300

    add templates to registrations

---
+++ appleused_project/email_info.py
@@ -0,0 +1,9 @@
+# email information for send maseges to users
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'd.protsiuk@gmail.com'
+EMAIL_HOST_PASSWORD = '05111994'
+# DEFAULT_FROM_EMAIL = 'Your name'
+# DEFAULT_TO_EMAIL = 'Your email'
